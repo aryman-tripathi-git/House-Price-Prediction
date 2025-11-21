3a576f57b54e13bc49b184955a044fa1e40107b2
Aryman Tripathi <tripathiaryman2@gmail.com>
2025-11-21 14:19:14 +0530

    folder structure and dependencies

---
+++ House Price Prediction/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn import metrics
+
+house_price_dataset = sklearn.datasets.fetch_california_housing()
+print(house_price_dataset)
