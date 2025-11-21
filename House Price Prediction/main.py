d3ac5f749dd4f6a45a7e3abceeb4ce5c9c680f53
Aryman Tripathi <tripathiaryman2@gmail.com>
2025-11-21 16:42:34 +0530

    final commit

--- House Price Prediction/main.py
+++ House Price Prediction/main.py
@@ -8,4 +8,25 @@ from xgboost import XGBRegressor
 from sklearn import metrics
 
 house_price_dataset = sklearn.datasets.fetch_california_housing()
-print(house_price_dataset)
+
+house_price_dataframe = pd.DataFrame(house_price_dataset.data, columns=house_price_dataset.feature_names)
+
+house_price_dataframe['Price'] = house_price_dataset.target
+
+X = house_price_dataframe.drop(['Price'], axis=1)
+Y = house_price_dataframe['Price']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+model = XGBRegressor()
+model.fit(X_train, Y_train)
+
+test_data_prediction = model.predict(X_test)
+score_3 = metrics.r2_score(Y_test, test_data_prediction)
+score_4 = metrics.mean_absolute_error(Y_test, test_data_prediction)
+print("R squared value : ", score_3)
+print("Mean Absolute Error : ", score_4)
+
+plt.scatter(Y_test, test_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Price vs Predicted Price")
+plt.show()
